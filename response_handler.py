348c8c18ff6e43a68381357b794c76a1ca9eafee
Youjin Song <ujinsong23@gmail.com>
2025-03-14 15:37:42 -0700

    feat: save time in easier format, split function

--- response_handler.py
+++ response_handler.py
@@ -31,7 +31,8 @@ def save_response(prompt_id, criteria_id, rating, batch_id, user_id, current_ind
         prompt_id,
         criteria_id,
         rating_json,
-        review_duration
+        review_duration,
+        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     )
     c.execute(
         "INSERT INTO evaluations (user_id, batch_id, current_index, prompt_id, criteria_id, rating, review_duration, clicked_video_count, clicked_video_unrepeated_count, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
@@ -45,7 +46,7 @@ def save_response(prompt_id, criteria_id, rating, batch_id, user_id, current_ind
             review_duration,
             st.session_state.clicked_video_count,
             len(st.session_state.clicked_video_ids),
-            time.time(),
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         ),
     )
     c.execute
@@ -60,16 +61,19 @@ def fetch_evaluations():
     conn.close()
     return rows
 
-def count_valid_user_responses(user_id):
+def fetch_valid_user_responses(user_id):
+
     conn = sqlite3.connect(osp.join(SAVE_PATH, "evaluations.db"))
     c = conn.cursor()
-    c.execute("SELECT DISTINCT user_id, batch_id, current_index, prompt_id, criteria_id, rating FROM evaluations WHERE user_id = ?", (user_id,))
+    c.execute("SELECT DISTINCT user_id, batch_id, current_index, prompt_id, criteria_id, rating, timestamp FROM evaluations WHERE user_id = ?", (user_id,))
     rows = c.fetchall()
     unique_pairs = set()
-    evaluated_indices = set()
+    evaluations = []
+
+    rows = sorted(rows, key=lambda x: x[-1], reverse=True)
     for row in rows:
-        user_id, batch_id, current_index, prompt_id, criteria_id, rating = row
-        if (prompt_id, criteria_id) not in unique_pairs:
+        user_id, batch_id, current_index, prompt_id, criteria_id, rating, timestamp = row
+        if (current_index, prompt_id, criteria_id) not in unique_pairs:
             try:
                 rating_list = json.loads(rating)
             except json.JSONDecodeError:
@@ -85,10 +89,15 @@ def count_valid_user_responses(user_id):
                 set(rating_list) == {1, 2, 3, 4} and
                 batch_id == user_id%NUM_BATCHES
             ):
-                evaluated_indices.add(current_index)
-                unique_pairs.add((prompt_id, criteria_id))
+                unique_pairs.add((current_index, prompt_id, criteria_id))
+                evaluations.append([user_id, batch_id, current_index, prompt_id, criteria_id, rating_list])
     conn.close()
+    return set([pair[0] for pair in unique_pairs]), evaluations
+
+def count_valid_user_responses(user_id):
+    evaluated_indices, evaluations = fetch_valid_user_responses(user_id)
     if len(evaluated_indices) > 0:
+        batch_id = evaluations[0][1]
         print(f"User {user_id} has evaluated {evaluated_indices} indices in batch {batch_id}")
     else:
         print(f"User {user_id} has not evaluated any indices yet")
